71459e6623ab36c871af172d979a8bf1e5bd511d
Eder Henrique de Souza Filho <souzafilhoeder@gmail.com>
2025-05-23 20:01:25 -0300

    Adiciona máscara automática para o CPF enquanto o usuário digita

---
+++ script.js
@@ -0,0 +1,13 @@
+// Espera o DOM carregar completamente antes de executar o código
+document.addEventListener('DOMContentLoaded', function() {
+// Obtém referências para os elementos do DOM
+    const cpfInput = document.getElementById('cpfInput');
+    const verifyButton = document.getElementById('verifyButton');
+    const clearButton = document.getElementById('clearButton');
+    const resultDiv = document.getElementById('result');
+    const statusMessage = document.getElementById('statusMessage');
+    const body = document.body;
+
+     // Adiciona máscara automática para o CPF enquanto o usuário digita
+    cpfInput.addEventListener('input', function(e) {});
+});
