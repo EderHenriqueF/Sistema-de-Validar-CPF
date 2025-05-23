dc28eb9aa4caba60a665096c5d5d9f9871879717
Eder Henrique de Souza Filho <souzafilhoeder@gmail.com>
2025-05-23 20:07:41 -0300

    Aplica a formatação do CPF (XXX.XXX.XXX-XX)

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Remove todos os caracteres não numéricos
         let value = e.target.value.replace(/\D/g, '');
+
+        // Aplica a formatação do CPF (XXX.XXX.XXX-XX)
+        if (value.length > 9) {
+            value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+        } else if (value.length > 6) {
+            value = value.replace(/(\d{3})(\d{3})(\d{1,3})/, '$1.$2.$3');
+        } else if (value.length > 3) {s
+            value = value.replace(/(\d{3})(\d{1,3})/, '$1.$2');
+        }
         
     });
 });
